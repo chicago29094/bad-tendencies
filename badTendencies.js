113a69814d0426e56fab15fcaf819cec51ff484c
chicago29094 <harry.anastopoulos@telusys.net>
2021-06-11 08:46:26 -0500

    Fix absolute paths, change to relative paths

--- badTendencies.js
+++ badTendencies.js
@@ -879,7 +879,7 @@ function moveBandMember(bandMember) {
             (willCollide[1]["collisionType"]==='banderMember4') ) {
                 //console.log(willCollide[1]["collisionType"]);
                 bounceBack=true;
-                bandmember.health=bandmember.health-2;
+                bandmMember.health=bandMember.health-2;
             }
             else {
                 bandMember.lastDirChange=0;
